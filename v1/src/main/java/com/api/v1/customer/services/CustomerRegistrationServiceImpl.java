26ef6b86e3cc367e6d9222532db947636b09a353
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-08 20:40:39 -0300

    Created the service CustomerRegistrationService

---
+++ v1/src/main/java/com/api/v1/customer/services/CustomerRegistrationServiceImpl.java
@@ -0,0 +1,34 @@
+package com.api.v1.customer.services;
+
+import com.api.v1.customer.CustomerResponseMapper;
+import com.api.v1.customer.builders.CustomerBuilder;
+import com.api.v1.customer.domain.Customer;
+import com.api.v1.customer.domain.CustomerRepository;
+import com.api.v1.customer.dtos.CustomerRegistrationRequestDto;
+import com.api.v1.customer.dtos.CustomerResponseDto;
+import com.api.v1.customer.exceptions.DuplicatedSsnException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+class CustomerRegistrationServiceImpl implements CustomerRegistrationService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public Mono<CustomerResponseDto> register(CustomerRegistrationRequestDto request) {
+        return customerRepository
+                .findAll()
+                .hasElements()
+                .flatMap(exists -> {
+                    if (exists) return Mono.error(new DuplicatedSsnException(request.ssn()));
+                    return Mono.defer(() -> {
+                        Customer customer = CustomerBuilder.create().fromDto(request).build();
+                        return customerRepository.save(customer);
+                    }).flatMap(savedCustomer -> Mono.just(CustomerResponseMapper.map(savedCustomer)));
+                });
+    }
+
+}
