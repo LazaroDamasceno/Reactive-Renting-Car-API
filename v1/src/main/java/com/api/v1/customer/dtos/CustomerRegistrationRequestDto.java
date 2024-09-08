ba24047bd72de32f5a1816ec39a8215a3b431e7e
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-08 20:32:57 -0300

    Changed CustomerRegistrationRequestDto

--- v1/src/main/java/com/api/v1/customer/dtos/CustomerRegistrationRequestDto.java
+++ v1/src/main/java/com/api/v1/customer/dtos/CustomerRegistrationRequestDto.java
@@ -1,15 +1,21 @@
 package com.api.v1.customer.dtos;
 
+import com.api.v1.customer.SSN;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public record CustomerRegistrationRequestDto(
-        String firstName,
+        @NotBlank String firstName,
         String middleName,
-        String lastName,
-        String ssn,
-        LocalDate birthDate,
-        String email,
-        String address,
-        String phoneNumber
+        @NotBlank String lastName,
+        @SSN String ssn,
+        @NotNull LocalDate birthDate,
+        @NotBlank @Email String email,
+        @NotBlank String address,
+        @NotBlank @Size(min=10, max=10) String phoneNumber
 ) {
 }
