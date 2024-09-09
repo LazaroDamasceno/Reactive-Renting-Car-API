e305460936fef854c8afb8b1a7be20c309cfd026
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-09 17:12:20 -0300

    Created the test class CustomerUpdatingTest

---
+++ v1/src/test/java/com/api/v1/CustomerUpdatingTest.java
@@ -0,0 +1,42 @@
+package com.api.v1;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CustomerUpdatingTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Order(1)
+    @Test
+    void testSuccessfulCustomerUpdating() {
+        webTestClient
+                .put()
+                .uri("api/v1/customers/%s".formatted("123456789"))
+                //.bodyValue()
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Order(2)
+    @Test
+    void testUnsuccessfulCustomerUpdating() {
+        webTestClient
+                .put()
+                .uri("api/v1/customers/%s".formatted("123456789"))
+                //.bodyValue()
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
