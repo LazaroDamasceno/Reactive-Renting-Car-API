acc6ffd762e2d36e04e8d0c5aa86432b4e57a6ef
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-09 21:04:03 -0300

    Created the entity Car

---
+++ v1/src/main/java/com/api/v1/car/domain/Car.java
@@ -0,0 +1,72 @@
+package com.api.v1.car.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Getter
+@Document(collection = "v1_cars")
+public class Car {
+
+    @Id
+    private UUID id;
+
+    @Field
+    private String make;
+
+    @Field
+    private String model;
+
+    @Field
+    private int productionYear;
+
+    @Field
+    private String vin;
+
+    @Field
+    private int plateNumber;
+
+    @Field
+    private String addedAt;
+
+    @Field
+    private String updatedAt;
+
+    public Car(
+            UUID id,
+            String make,
+            String model,
+            int productionYear,
+            String vin,
+            int plateNumber,
+            String addedAt
+    ) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.productionYear = productionYear;
+        this.vin = vin;
+        this.plateNumber = plateNumber;
+        this.addedAt = addedAt;
+    }
+
+    public void update(
+            String make,
+            String model,
+            int productionYear,
+            String vin,
+            int plateNumber
+    ) {
+        this.make = make;
+        this.model = model;
+        this.productionYear = productionYear;
+        this.vin = vin;
+        this.plateNumber = plateNumber;
+        this.updatedAt = ZonedDateTime.now().toString();
+    }
+
+}
