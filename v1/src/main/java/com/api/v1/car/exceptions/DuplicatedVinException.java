a12221efbb099a6ae2d79c1c99f1cac495cdf0c9
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-09 23:02:59 -0300

    Fixed the exception DuplicatedVinException

--- v1/src/main/java/com/api/v1/car/exceptions/DuplicatedVinException.java
+++ v1/src/main/java/com/api/v1/car/exceptions/DuplicatedVinException.java
@@ -3,6 +3,6 @@ package com.api.v1.car.exceptions;
 public class DuplicatedVinException extends RuntimeException {
 
     public DuplicatedVinException(String vin) {
-        super("The input VIN is already registered.".formatted(vin));
+        super("The input VIN is %s already registered.".formatted(vin));
     }
 }
