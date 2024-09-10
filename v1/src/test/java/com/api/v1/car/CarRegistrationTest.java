24ae739c993673a5fa6ba32a9e0be22bda778787
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-09 23:10:19 -0300

    Created the test class CarRegistrationTest

---
+++ v1/src/test/java/com/api/v1/car/CarRegistrationTest.java
@@ -0,0 +1,42 @@
+package com.api.v1.car;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CarRegistrationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void testSuccessfulCarRegistration() {
+        webTestClient
+                .post()
+                .uri("api/v1/cars")
+                //.bodyValue()
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    @Order(1)
+    void testUnsuccessfulCarRegistration() {
+        webTestClient
+                .post()
+                .uri("api/v1/cars")
+                //.bodyValue()
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
