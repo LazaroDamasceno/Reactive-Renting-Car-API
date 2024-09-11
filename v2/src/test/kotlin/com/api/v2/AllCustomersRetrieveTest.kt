e0615fce9fe99bcb4b8473203f6ef00507aacba1
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-11 16:41:45 -0300

    Changed AllCustomersRetrieveTest

--- v2/src/test/kotlin/com/api/v2/AllCustomersRetrieveTest.kt
+++ v2/src/test/kotlin/com/api/v2/AllCustomersRetrieveTest.kt
@@ -1,14 +1,11 @@
 package com.api.v2
 
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
+
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AllCustomersRetrieveTest {
 
@@ -16,25 +13,13 @@ class AllCustomersRetrieveTest {
     private lateinit var webTestClient: WebTestClient
 
     @Test
-    @Order(1)
-    fun testSuccessfulAllCustomerRetrieve() {
+    fun testIfCustomerIsNotEmpty() {
         webTestClient
             .get()
             .uri("api/v1/customers/${123456789}")
             .exchange()
             .expectStatus()
-            .is2xxSuccessful
-    }
-
-    @Test
-    @Order(2)
-    fun testUnsuccessfulAllCustomerRetrieve() {
-        webTestClient
-            .get()
-            .uri("api/v1/customers/${123456789}")
-            .exchange()
-            .expectStatus()
-            .is5xxServerError
+            .is2xxSuccessful()
     }
 
 }
