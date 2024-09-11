0664f56b0ce98be3ecc70036bc8e17cf86cefae8
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-11 19:20:28 -0300

    Created the controller AllCustomersRetrievalController

---
+++ v2/src/main/kotlin/com/api/v2/customer/controllers/AllCustomersRetrievalController.kt
@@ -0,0 +1,29 @@
+package com.api.v2.customer.controllers
+
+import com.api.v2.customer.dtos.CustomerResponseDto
+import com.api.v2.customer.services.AllCustomersRetrievalService
+import kotlinx.coroutines.flow.Flow
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("api/v1/customers")
+class AllCustomersRetrievalController {
+
+    @Autowired
+    private lateinit var service: AllCustomersRetrievalService
+
+    @GetMapping("{ssn}")
+    @ResponseStatus(value = HttpStatus.OK)
+    suspend fun findBySsn(@PathVariable ssn: String): CustomerResponseDto {
+        return service.findBySsn(ssn)
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    suspend fun findAll(): Flow<CustomerResponseDto> {
+        return service.findAll()
+    }
+
+}
