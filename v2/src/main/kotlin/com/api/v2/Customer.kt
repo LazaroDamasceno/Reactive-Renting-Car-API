f33fa7ac02ecff104e8094753e6720810a75e781
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-10 22:24:47 -0300

    Created the builder CustomerBuilder

--- v2/src/main/kotlin/com/api/v2/Customer.kt
+++ v2/src/main/kotlin/com/api/v2/Customer.kt
@@ -22,51 +22,32 @@ class Customer {
 
     constructor(
         id: UUID,
-        createdAt: String,
-        phoneNumber: String,
-        address: String,
-        email: String,
-        birthDate: LocalDate,
-        ssn: String,
-        lastName: String,
+        firstName: String,
         middleName: String,
-        firstName: String
+        lastName: String,
+        ssn: String,
+        birthDate: LocalDate,
+        email: String,
+        address: String,
+        phoneNumber: String,
+        createdAt: String
     ) {
         this.id = id
-        this.createdAt = createdAt
-        this.phoneNumber = phoneNumber
-        this.address = address
-        this.email = email
-        this.birthDate = birthDate
-        this.ssn = ssn
-        this.lastName = lastName
-        this.middleName = middleName
         this.firstName = firstName
+        this.middleName = middleName
+        this.lastName = lastName
+        this.ssn = ssn
+        this.birthDate = birthDate
+        this.email = email
+        this.address = address
+        this.phoneNumber = phoneNumber
+        this.createdAt = createdAt
     }
+
 
     fun fullName(): String {
         if (middleName.isEmpty()) return "$firstName $lastName"
         return "$firstName $middleName $lastName"
-    }
-
-    fun update(
-        phoneNumber: String,
-        address: String,
-        email: String,
-        birthDate: LocalDate,
-        lastName: String,
-        middleName: String,
-        firstName: String
-    ) {
-
-        this.phoneNumber = phoneNumber
-        this.address = address
-        this.email = email
-        this.birthDate = birthDate
-        this.lastName = lastName
-        this.middleName = middleName
-        this.firstName = firstName
-        this.updatedAt = ZonedDateTime.now().toString()
     }
 
 }
---
+++ v2/src/main/kotlin/com/api/v2/CustomerBuilder.kt
@@ -0,0 +1,53 @@
+package com.api.v2
+
+import jakarta.validation.Valid
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.util.*
+
+class CustomerBuilder {
+
+    private val id = UUID.randomUUID();
+    private lateinit var firstName: String
+    private lateinit var middleName: String
+    private lateinit var lastName: String
+    private lateinit var ssn: String
+    private lateinit var birthDate: LocalDate
+    private lateinit var email: String
+    private lateinit var address: String
+    private lateinit var phoneNumber: String
+    private val createdAt = ZonedDateTime.now().toString()
+
+    companion object {
+        fun create(): CustomerBuilder {
+            return CustomerBuilder()
+        }
+    }
+
+    fun fromDto(@Valid requestDto: CustomerRegistrationRequestDto) {
+        this.firstName = requestDto.firstName
+        this.middleName = requestDto.middleName
+        this.lastName = requestDto.lastName
+        this.ssn = requestDto.ssn
+        this.birthDate = requestDto.birthDate
+        this.email = requestDto.email
+        this.address = requestDto.address
+        this.phoneNumber = requestDto.phoneNumber
+    }
+
+    fun build(): Customer {
+        return Customer(
+            id,
+            firstName,
+            middleName,
+            lastName,
+            ssn,
+            LocalDate.parse("2000-12-12"),
+            email,
+            address,
+            phoneNumber,
+            createdAt
+        )
+    }
+    
+}
