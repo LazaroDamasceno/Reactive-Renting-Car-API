1852fd6434774f78b25f705f6b9794d2a785052f
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-11 16:37:35 -0300

    Changed the test class CustomerRetrieveTest

---
+++ v2/src/test/kotlin/com/api/v2/CustomerRetrieveTest.kt
@@ -0,0 +1,40 @@
+package com.api.v2
+
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CustomerRetrieveTest {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @Test
+    @Order(1)
+    fun testSuccessfulCustomerRetrieveBySsn() {
+        webTestClient
+            .get()
+            .uri("api/v1/customers/${123456789}")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful
+    }
+
+    @Test
+    @Order(2)
+    fun testUnsuccessfulCustomerRetrieveBySsn() {
+        webTestClient
+            .get()
+            .uri("api/v1/customers/${123456789}")
+            .exchange()
+            .expectStatus()
+            .is5xxServerError
+    }
+
+}
