2259600384196b450cd6dad13af9960569991efa
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-11 13:16:23 -0300

    Created the dto CustomerUpdateRequestDto

---
+++ v2/src/main/kotlin/com/api/v2/CustomerUpdateRequestDto.kt
@@ -0,0 +1,13 @@
+package com.api.v2
+
+import java.time.LocalDate
+
+data class CustomerUpdateRequestDto(
+    var firstName: String,
+    var middleName: String,
+    var lastName: String,
+    var birthDate: LocalDate,
+    var email: String,
+    var phoneNumber: String,
+    var gender: String,
+)
