ccdbff3ed6c53e3064593487ec8beef058bc3692
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-11 00:14:40 -0300

    Deleted v2

--- v2/src/main/kotlin/com/api/v2/CustomerRegistrationService.kt
+++
@@ -1,9 +0,0 @@
-package com.api.v2
-
-import reactor.core.publisher.Mono
-
-interface CustomerRegistrationService {
-
-    suspend fun register(requestDto: CustomerRegistrationRequestDto): Mono<CustomerResponseDto>
-
-}
--- v2/src/main/kotlin/com/api/v2/DuplicatedSsnException.kt
+++
@@ -1,7 +0,0 @@
-package com.api.v2
-
-import java.lang.RuntimeException
-
-class DuplicatedSsnException(ssn: String): RuntimeException(
-    "The given SSN $ssn is already registered."
-)
