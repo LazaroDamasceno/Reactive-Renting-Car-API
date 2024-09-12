d4fbe143e68c0e1f9b5ce3d0ed259e028c0139a0
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-12 15:59:07 -0300

    Created the entity CreditCard

---
+++ v2/src/main/kotlin/com/api/v2/creditcard/CreditCard.kt
@@ -0,0 +1,10 @@
+package com.api.v2.creditcard
+
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "v2_credit_cards")
+data class CreditCard(
+    val ownerName: String,
+    val cardNumber: String,
+    val addedAt: String
+)
