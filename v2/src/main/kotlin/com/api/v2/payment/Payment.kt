a4eb04bfc0f06b799c364ddc7d00bcb403016582
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-12 16:22:00 -0300

    Fixed an issue

--- v2/src/main/kotlin/com/api/v2/payment/Payment.kt
+++ v2/src/main/kotlin/com/api/v2/payment/Payment.kt
@@ -5,9 +5,8 @@ import com.api.v2.rent.Rent;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "v1_payments")
-public record Payment(
-        CreditCard creditCard,
-        String paidAt,
-        Rent rent
-) {
-}
+data class Payment(
+    val creditCard: CreditCard,
+    val paidAt: String,
+    val rent: Rent
+)
--- v2/src/main/kotlin/com/api/v2/rent/Rent.kt
+++ v2/src/main/kotlin/com/api/v2/rent/Rent.kt
@@ -1,47 +1,29 @@
 package com.api.v2.rent;
 
-import com.api.v2.car.domain.Car;
-import com.api.v2.customer.domain.Customer;
-import com.api.v2.payment.Payment;
+import com.api.v2.car.domain.Car
+import com.api.v2.customer.domain.Customer
+import com.api.v2.payment.Payment
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Document(collection = "v1_rents")
-public class Rent {
+class Rent(
+    @Id
+    val id: UUID,
+    val car: Car,
+    val customer: Customer,
+    val payment: Payment,
+    val rentedAt: String,
+    val dueDate: String
 
-    private final UUID id;
+) {
 
-    private final Car car;
+    var returnedAt: String? = null
 
-    private final Customer customer;
-
-    private final Payment payment;
-
-    private final String rentedAt;
-
-    private final String dueDate;
-
-    private String returnedAt;
-
-    public Rent(
-            UUID id,
-            Car car,
-            Customer customer,
-            Payment payment,
-            String rentedAt,
-            String dueDate
-    ) {
-        this.id = id;
-        this.car = car;
-        this.customer = customer;
-        this.payment = payment;
-        this.rentedAt = rentedAt;
-        this.dueDate = dueDate;
-    }
-
-    public void markAsReturned() {
+    fun markAsReturned() {
         returnedAt = ZonedDateTime.now().toString();
     }
 
