045a75f94f6c1c8b332e558047d15b37d54d03ce
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-12 17:00:27 -0300

    Added attributes flag (String), cvc (String) and dueDate (String) to CreditCard

--- v2/src/main/kotlin/com/api/v2/creditcard/CreditCard.kt
+++ v2/src/main/kotlin/com/api/v2/creditcard/CreditCard.kt
@@ -1,10 +1,14 @@
 package com.api.v2.creditcard
 
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
 
 @Document(collection = "v1_credit_cards")
 data class CreditCard(
     val ownerName: String,
     val cardNumber: String,
+    val flag: String,
+    val cvc: String,
+    val dueDate: LocalDate,
     val addedAt: String
 )
