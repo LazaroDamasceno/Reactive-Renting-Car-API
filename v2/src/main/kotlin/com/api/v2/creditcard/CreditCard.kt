5fd528ca59220604f2edaca303acf3b293d11cca
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-12 16:11:49 -0300

    Fixed Payment

--- v2/src/main/kotlin/com/api/v2/creditcard/CreditCard.kt
+++ v2/src/main/kotlin/com/api/v2/creditcard/CreditCard.kt
@@ -2,7 +2,7 @@ package com.api.v2.creditcard
 
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document(collection = "v2_credit_cards")
+@Document(collection = "v1_credit_cards")
 data class CreditCard(
     val ownerName: String,
     val cardNumber: String,
--- v2/src/main/kotlin/com/api/v2/payment/Payment.java
+++ v2/src/main/kotlin/com/api/v2/payment/Payment.java
@@ -1,6 +1,7 @@
 package com.api.v2.payment;
 
 import com.api.v2.creditcard.CreditCard;
+import com.api.v2.rent.Rent;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "v1_payments")
