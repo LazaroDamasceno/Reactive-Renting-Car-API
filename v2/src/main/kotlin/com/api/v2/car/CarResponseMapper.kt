12e0b7b90b9695414f089c7a1efdcc4fe99781c9
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-11 22:23:22 -0300

    Created the mapper CarResponseMapper

---
+++ v2/src/main/kotlin/com/api/v2/car/CarResponseMapper.kt
@@ -0,0 +1,19 @@
+package com.api.v2.car
+
+class CarResponseMapper {
+
+    companion object {
+        fun mapToDto(car: Car): CarResponseDto {
+            return CarResponseDto(
+                car.vin,
+                car.model,
+                car.productionYear,
+                car.make,
+                car.plateNumber,
+                car.createdAt,
+                car.updateAt
+            )
+        }
+    }
+
+}
