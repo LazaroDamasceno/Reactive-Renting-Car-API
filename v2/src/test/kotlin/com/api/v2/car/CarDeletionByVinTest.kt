d96d104762786ffcbb2af974858f8e7204dce6d8
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-12 13:08:54 -0300

    Created the (not fully implemented) test classes AllCarsDeletionTest, AllCarsRetrievalTest, CarDeletionByVinTest and CarRetrievalByVinTest

---
+++ v2/src/test/kotlin/com/api/v2/car/CarDeletionByVinTest.kt
@@ -0,0 +1,16 @@
+package com.api.v2.car
+
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+private class CarDeletionByVinTest {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+}
