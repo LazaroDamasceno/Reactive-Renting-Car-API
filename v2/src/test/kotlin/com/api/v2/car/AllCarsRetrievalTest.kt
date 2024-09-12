31c31b6a58a392ce14f8c4b1c8de45df60049b82
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-12 14:16:47 -0300

    Implemented CarRetrievalByVinTest and AllCarsRetrievalTest

--- v2/src/test/kotlin/com/api/v2/car/AllCarsRetrievalTest.kt
+++ v2/src/test/kotlin/com/api/v2/car/AllCarsRetrievalTest.kt
@@ -1,16 +1,24 @@
 package com.api.v2.car
 
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 private class AllCarsRetrievalTest {
 
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun testIfCarHasElements() {
+        webTestClient
+            .get()
+            .uri("api/v2/cars")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+    }
 
 }
--- v2/src/test/kotlin/com/api/v2/car/CarRetrievalByVinTest.kt
+++ v2/src/test/kotlin/com/api/v2/car/CarRetrievalByVinTest.kt
@@ -1,6 +1,8 @@
 package com.api.v2.car
 
 import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -12,5 +14,29 @@ private class CarRetrievalByVinTest {
 
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @Test
+    @Order(1)
+    fun testSuccessfulCarRetrieval() {
+        val vin = "12345678901234567"
+        webTestClient
+            .get()
+            .uri("api/v2/cars/$vin")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+    }
+
+    @Test
+    @Order(2)
+    fun testUnsuccessfulCarRetrieval() {
+        val vin = "12345678901234566"
+        webTestClient
+            .get()
+            .uri("api/v2/cars/$vin")
+            .exchange()
+            .expectStatus()
+            .is5xxServerError()
+    }
 
 }
