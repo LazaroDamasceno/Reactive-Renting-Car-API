69e2290d1351d53784a954240db4b0b5b955e4c0
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-12 22:39:30 -0300

    Created the controller AllCreditCardDeletionController

---
+++ v2/src/main/kotlin/com/api/v2/creditcard/controllers/AllCreditCardDeletionController.kt
@@ -0,0 +1,37 @@
+package com.api.v2.creditcard.controllers
+
+import com.api.v2.creditcard.services.AllCreditCardDeletionService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1/credit-cards")
+class AllCreditCardDeletionController {
+
+    @Autowired
+    private lateinit var service: AllCreditCardDeletionService
+
+    @DeleteMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    suspend fun deleteAll() {
+        return service.deleteAll()
+    }
+
+    @DeleteMapping("by-customer/{ssn}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    suspend fun deleteAllByCustomer(@PathVariable ssn: String) {
+        return service.deleteAllByCustomer(ssn)
+    }
+
+    @DeleteMapping("by-card-number/{cardNumber}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    suspend fun deleteByCardNumber(@PathVariable cardNumber: String) {
+        return service.deleteByCardNumber(cardNumber)
+    }
+
+}
