e6aef1b4b1d92b87a5f455c6d03623e572bc58a6
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-13 00:16:19 -0300

    Created the test class AllCreditCardsRetrievalTest

---
+++ v2/src/test/kotlin/com/api/v2/creditcar/AllCreditCardsRetrievalTest.kt
@@ -0,0 +1,24 @@
+package com.api.v2.creditcar
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+private class AllCreditCardsRetrievalTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun testSuccessfulAllCreditCardsRetrieval() {
+        webTestClient
+            .get()
+            .uri("api/v2/credit-cards")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+    }
+
+}
