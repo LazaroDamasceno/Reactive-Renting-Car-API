ee14c7297d5e471079b7b17e89caaae39b31f337
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-13 14:09:12 -0300

    Created the test class CreditCardDeletionTest.java

--- v1/src/test/java/com/api/v1/creditcard/CreditCardDeletionTest.java
+++ v1/src/test/java/com/api/v1/creditcard/CreditCardDeletionTest.java
@@ -1,25 +1,40 @@
 package com.api.v1.creditcard;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class CreditCardRetrievalTest {
+class CreditCardDeletionTest {
 
     @Autowired
     WebTestClient webTestClient;
 
     @Test
-    void testIfCreditCardIsNotEmpty() {
+    @Order(1)
+    void testSuccessfulCreditCardDeletion() {
         webTestClient
-                .get()
+                .delete()
                 .uri("api/v1/credit-cars")
                 .exchange()
                 .expectStatus()
                 .is2xxSuccessful();
+    }
+
+    @Test
+    @Order(2)
+    void testUnsuccessfulCreditCardDeletion() {
+        webTestClient
+                .delete()
+                .uri("api/v1/credit-cars")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
     }
 
 }
