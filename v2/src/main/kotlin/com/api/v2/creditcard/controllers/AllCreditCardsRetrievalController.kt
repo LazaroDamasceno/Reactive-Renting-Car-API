bc7e7fe3f72f7c50721c273a2a773deae5d418ca
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-13 00:53:40 -0300

    Fixed the endpoint AllCreditCardsRetrievalController

--- v2/src/main/kotlin/com/api/v2/creditcard/controllers/AllCreditCardsRetrievalController.kt
+++ v2/src/main/kotlin/com/api/v2/creditcard/controllers/AllCreditCardsRetrievalController.kt
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("api/v1/credit-cards")
+@RequestMapping("api/v2/credit-cards")
 class AllCreditCardsRetrievalController {
 
     @Autowired
