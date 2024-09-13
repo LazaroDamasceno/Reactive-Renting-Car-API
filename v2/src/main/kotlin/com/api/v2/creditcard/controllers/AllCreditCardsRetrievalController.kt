1b0d21c48adf2f8e44b0790301ddcd7754dbda36
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-13 00:42:31 -0300

    Created the controller AllCreditCardsRetrievalController

---
+++ v2/src/main/kotlin/com/api/v2/creditcard/controllers/AllCreditCardsRetrievalController.kt
@@ -0,0 +1,29 @@
+package com.api.v2.creditcard.controllers
+
+import com.api.v2.creditcard.dtos.CreditCardResponseDto
+import com.api.v2.creditcard.services.AllCreditCardsRetrievalService
+import kotlinx.coroutines.flow.Flow
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("api/v1/credit-cards")
+class AllCreditCardsRetrievalController {
+
+    @Autowired
+    private lateinit var service: AllCreditCardsRetrievalService
+
+    @GetMapping("{cardNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    suspend fun findByCardNumber(@PathVariable cardNumber: String): CreditCardResponseDto {
+        return service.findByCardNumber(cardNumber)
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    suspend fun findAll(): Flow<CreditCardResponseDto> {
+        return service.findAll()
+    }
+
+}
