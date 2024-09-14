2e510bdbaa164a65375db9540e24cbc820b64857
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-14 17:56:41 -0300

    Created the controller AllPaymentsRetrievalController

---
+++ v1/src/main/java/com/api/v1/controllers/AllPaymentsRetrievalController.java
@@ -0,0 +1,30 @@
+package com.api.v1.controllers;
+
+import com.api.v1.dtos.PaymentResponseDto;
+import com.api.v1.services.AllPaymentsRetrievalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequestMapping("api/v1/payments")
+@RestController
+public class AllPaymentsRetrievalController {
+
+    @Autowired
+    private AllPaymentsRetrievalService service;
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<PaymentResponseDto> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("{orderNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Mono<PaymentResponseDto> findByOrderNumber(@PathVariable String orderNumber) {
+        return service.findByOrderNumber(orderNumber);
+    }
+
+}
