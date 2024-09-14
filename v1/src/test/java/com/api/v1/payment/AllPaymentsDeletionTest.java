b365e37afa4738f65790d85b194883f8d4913b15
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-14 16:33:20 -0300

    Created the test class AllPaymentsDeletionTest

---
+++ v1/src/test/java/com/api/v1/payment/AllPaymentsDeletionTest.java
@@ -0,0 +1,39 @@
+package com.api.v1.payment;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AllPaymentsDeletionTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void tesSuccessfulAllPaymentsTest() {
+        webTestClient
+                .delete()
+                .uri("api/v1/payments")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    void tesUnsuccessfulAllPaymentsTest() {
+        webTestClient
+                .delete()
+                .uri("api/v1/payments")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+}
