16547f75aa0be8cdf66150d2a0b9ce4b551fd332
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-14 19:11:27 -0300

    Changed the test class PaymentRetrievalByOrderNumberTest

--- v2/src/test/kotlin/com/api/v2/payment/PaymentRetrievalByOrderNumberTest.kt
+++ v2/src/test/kotlin/com/api/v2/payment/PaymentRetrievalByOrderNumberTest.kt
@@ -1,6 +1,6 @@
 package com.api.v2.payment
 
-import org.junit.jupiter.api.MethodOrderer
+import  org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
