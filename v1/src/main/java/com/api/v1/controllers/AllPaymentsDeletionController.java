80b03aa4d2b109dad83cab459d4f42b5f4962dc9
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-14 18:09:05 -0300

    Created the controller AllPaymentsDeletionController

---
+++ v1/src/main/java/com/api/v1/controllers/AllPaymentsDeletionController.java
@@ -0,0 +1,28 @@
+package com.api.v1.controllers;
+
+import com.api.v1.services.AllPaymentsDeletionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/payments")
+public class AllPaymentsDeletionController {
+
+    @Autowired
+    private AllPaymentsDeletionService service;
+
+    @DeleteMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteAll() {
+        return service.deleteAll();
+    }
+
+    @DeleteMapping("{orderNumber}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteByOrderNumber(@PathVariable String orderNumber) {
+        return service.deleteByOrderNumber(orderNumber);
+    }
+
+}
