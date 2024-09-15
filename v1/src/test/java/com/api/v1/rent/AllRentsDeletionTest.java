762670f08a1cecb1feba1114602adec30429311f
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-15 20:54:23 -0300

    Created the test classes AllRentsRetrievalTest and AllRentsDeletionTest

---
+++ v1/src/test/java/com/api/v1/rent/AllRentsDeletionTest.java
@@ -0,0 +1,39 @@
+package com.api.v1.rent;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder((MethodOrderer.OrderAnnotation.class))
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AllRentsDeletionTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void testSuccessfulAllRentsDeletion() {
+        webTestClient
+                .delete()
+                .uri("api/v1/rents")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    void testUnsuccessfulAllRentsDeletion() {
+        webTestClient
+                .delete()
+                .uri("api/v1/rents")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
---
+++ v1/src/test/java/com/api/v1/rent/AllRentsRetrievalTest.java
@@ -0,0 +1,39 @@
+package com.api.v1.rent;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder((MethodOrderer.OrderAnnotation.class))
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AllRentsRetrievalTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void testSuccessfulAllRentsRetrieval() {
+        webTestClient
+                .get()
+                .uri("api/v1/rents")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    void testUnsuccessfulAllRentsRetrieval() {
+        webTestClient
+                .get()
+                .uri("api/v1/rents")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+}
