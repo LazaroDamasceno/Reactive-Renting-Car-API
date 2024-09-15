51144b588de8fe2cca9a37ae512d6dc993e0444e
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-15 01:31:28 -0300

    Fixed the interface RentRegistrationService

---
+++ v1/src/main/java/com/api/v1/services/RentRegistrationService.java
@@ -0,0 +1,11 @@
+package com.api.v1.services;
+
+import com.api.v1.domain.Rent;
+import com.api.v1.dtos.RentResponseDto;
+import reactor.core.publisher.Mono;
+
+public interface RentRegistrationService {
+
+    Mono<RentResponseDto> register(String ssn, String vin, String paymentOrderNumber);
+
+}
