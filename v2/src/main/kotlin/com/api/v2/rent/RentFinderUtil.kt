5ba4907ada22dce0a35b502ee1fe34a13e595d8a
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-15 23:12:46 -0300

    Created the util RentFinderUtil

---
+++ v2/src/main/kotlin/com/api/v2/rent/RentFinderUtil.kt
@@ -0,0 +1,30 @@
+package com.api.v2.rent
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.singleOrNull
+import kotlinx.coroutines.withContext
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.math.BigInteger
+
+@Component
+class RentFinderUtil {
+
+    @Autowired
+    private lateinit var repository: RentRepository
+
+    suspend fun find(orderNumber: String): Rent {
+        return withContext(Dispatchers.IO) {
+            val existingRent = repository
+                .findAll()
+                .filter { e -> e.orderNumber == BigInteger(orderNumber) }
+                .singleOrNull()
+            if (existingRent == null) {
+                throw RentNotFoundException(orderNumber)
+            }
+            existingRent
+        }
+    }
+
+}
