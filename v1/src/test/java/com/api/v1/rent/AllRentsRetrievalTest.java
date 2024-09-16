d3bc04bfd0a953918b3773ee9c7c38fdef0276a2
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-15 21:32:38 -0300

    Changed some test classes

--- v1/src/test/java/com/api/v1/rent/AllRentsRetrievalTest.java
+++ v1/src/test/java/com/api/v1/rent/AllRentsRetrievalTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@TestMethodOrder((MethodOrderer.OrderAnnotation.class))
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AllRentsRetrievalTest {
 
