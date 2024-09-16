f883aac2f96973c65b08501747224c626b95afe6
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-16 13:43:03 -0300

    Created the test RentRetrievalByOrderNumberTest

---
+++ v2/src/test/kotlin/com/api/v2/rent/RentRetrievalByOrderNumberTest.kt
@@ -0,0 +1,39 @@
+package com.api.v2.rent
+
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+private class RentRetrievalByOrderNumberTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    @Order(1)
+    fun testSuccessfulRetrieval() {
+        webTestClient
+            .get()
+            .uri("api/v2/rents/${2024001}")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+    }
+
+    @Test
+    fun testUnsuccessfulRetrieval() {
+        webTestClient
+            .get()
+            .uri("api/v2/rents/${2024000}")
+            .exchange()
+            .expectStatus()
+            .is5xxServerError()
+    }
+
+}
