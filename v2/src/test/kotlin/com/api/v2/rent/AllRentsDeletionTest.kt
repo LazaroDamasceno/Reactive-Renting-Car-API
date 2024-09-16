16f4d5259fc92d5b85ae3d70678b68164e3f1322
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-16 13:46:09 -0300

    Created the test AllRentsDeletionTest

---
+++ v2/src/test/kotlin/com/api/v2/rent/AllRentsDeletionTest.kt
@@ -0,0 +1,39 @@
+package com.api.v2.rent
+
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+private class AllRentsDeletionTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    @Order(1)
+    fun testSuccessfulDeletion() {
+        webTestClient
+            .delete()
+            .uri("api/v2/rents")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+    }
+
+    @Test
+    fun testUnsuccessfulDeletion() {
+        webTestClient
+            .delete()
+            .uri("api/v2/rents")
+            .exchange()
+            .expectStatus()
+            .is5xxServerError()
+    }
+
+}
