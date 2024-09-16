7c69ea33bd546876d978b12d9c95873c1b1f1165
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-15 22:23:39 -0300

    Created the repository RentRepository

---
+++ v2/src/main/kotlin/com/api/v2/rent/rent/RentRepository.kt
@@ -0,0 +1,6 @@
+package com.api.v2.rent.rent
+
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import java.util.UUID
+
+interface RentRepository: CoroutineCrudRepository<Rent, UUID>
