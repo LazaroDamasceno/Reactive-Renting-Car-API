a9c9f0f402c463418ab2ccf94870cd2fb98e44cd
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-15 21:53:56 -0300

    Created the controller AllRentsRetrievalController and the service AllRentsRetrievalService

---
+++ v1/src/main/java/com/api/v1/controllers/AllRentsRetrievalController.java
@@ -0,0 +1,30 @@
+package com.api.v1.controllers;
+
+import com.api.v1.dtos.RentResponseDto;
+import com.api.v1.services.AllRentsRetrievalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/rents")
+public class AllRentsRetrievalController {
+
+    @Autowired
+    private AllRentsRetrievalService service;
+
+    @GetMapping("{orderNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Mono<RentResponseDto> findByOrderNumber(@PathVariable String orderNumber) {
+        return service.findByOrderNumber(orderNumber);
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<RentResponseDto> findAll() {
+        return service.findAll();
+    }
+
+}
