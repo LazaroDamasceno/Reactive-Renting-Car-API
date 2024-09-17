268795e709a3d76bc15e1d38788bf55928f04ace
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-17 01:02:51 -0300

    Altered AllPaymentsDeletionServiceImpl.kt

--- kotlin/v2/src/main/kotlin/com/api/v2/payment/services/AllPaymentsDeletionServiceImpl.kt
+++ kotlin/v2/src/main/kotlin/com/api/v2/payment/services/AllPaymentsDeletionServiceImpl.kt
@@ -1,8 +1,10 @@
 package com.api.v2.payment.services
 
 import com.api.v2.payment.domain.PaymentRepository
+import com.api.v2.payment.dtos.EmptyPaymentEntity
 import com.api.v2.payment.utils.PaymentFinderUtil
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.withContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -19,7 +21,7 @@ private class AllPaymentsDeletionServiceImpl: AllPaymentsDeletionService {
     override suspend fun deleteAll() {
         withContext(Dispatchers.IO) {
             if (paymentRepository.findAll().count() == 0) {
-
+                throw EmptyPaymentEntity()
             }
             paymentRepository.deleteAll()
         }
